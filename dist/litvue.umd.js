4a9ba9a84e2cec016d64379fc0d5515cadfb15e3
Devesh Majithiya <deveshmajithiya@gofynd.com>
2022-10-13 18:47:03 +0530

    build updated (dist updated)

--- dist/litvue.umd.js
+++ dist/litvue.umd.js
@@ -96,10 +96,28 @@ return /******/ (function(modules) { // webpackBootstrap
 /************************************************************************/
 /******/ ({
 
-/***/ "2da1":
+/***/ "0bcb":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_LitButton_vue_vue_type_style_index_0_id_42558152_prod_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("2967");
+/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_LitButton_vue_vue_type_style_index_0_id_42558152_prod_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_mini_css_extract_plugin_dist_loader_js_ref_8_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_8_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_8_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_8_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_LitButton_vue_vue_type_style_index_0_id_42558152_prod_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__);
+/* unused harmony reexport * */
+
+
+/***/ }),
+
+/***/ "2967":
 /***/ (function(module, exports, __webpack_require__) {
 
 // extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "b9f2":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/button-loader.d555483e.gif";
 
 /***/ }),
 
@@ -146,6 +164,13 @@ return /******/ (function(modules) { // webpackBootstrap
 
 /***/ }),
 
+/***/ "fa5e":
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/loader.d50ec8f0.gif";
+
+/***/ }),
+
 /***/ "fae3":
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
@@ -173,24 +198,111 @@ if (typeof window !== 'undefined') {
 // Indicate to webpack that this file can be concatenated
 /* harmony default export */ var setPublicPath = (null);
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"5f6d8322-vue-loader-template"}!./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Button/LitButton.vue?vue&type=template&id=bf61b33e&scoped=true&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"5f6d8322-vue-loader-template"}!./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Button/LitButton.vue?vue&type=template&id=42558152&scoped=true&
 var render = function render() {
   var _vm = this,
     _c = _vm._self._c;
-  return _c('button', [_vm._t("default")], 2);
+  return _c('button', _vm._g({
+    staticClass: "lit-button",
+    class: _vm.classObject,
+    style: _vm.btnStyles,
+    attrs: {
+      "disabled": _vm.disabled
+    }
+  }, _vm.$listeners), [_c('div', {
+    directives: [{
+      name: "show",
+      rawName: "v-show",
+      value: !_vm.loading,
+      expression: "!loading"
+    }],
+    staticClass: "wrapper"
+  }, [_vm._t("default")], 2), _c('img', {
+    directives: [{
+      name: "show",
+      rawName: "v-show",
+      value: _vm.loading,
+      expression: "loading"
+    }],
+    staticClass: "loader",
+    attrs: {
+      "src": _vm.getLoader
+    }
+  })]);
 };
 var staticRenderFns = [];
 
-// CONCATENATED MODULE: ./src/components/Button/LitButton.vue?vue&type=template&id=bf61b33e&scoped=true&
+// CONCATENATED MODULE: ./src/components/Button/LitButton.vue?vue&type=template&id=42558152&scoped=true&
 
 // CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Button/LitButton.vue?vue&type=script&lang=js&
 /* harmony default export */ var LitButtonvue_type_script_lang_js_ = ({
-  name: "lit-button"
+  name: "lit-button",
+  props: {
+    variant: {
+      type: String,
+      default: "primary"
+    },
+    color: {
+      type: String
+    },
+    size: {
+      type: String,
+      default: "medium"
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    rounded: {
+      type: Boolean,
+      default: false
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    bordered: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    classObject: function classObject() {
+      return {
+        rounded: this.rounded,
+        primary: this.variant == "primary",
+        secondary: this.variant == "secondary",
+        large: this.size == "large",
+        medium: this.size == "medium",
+        small: this.size == "small",
+        disabled: this.disabled
+      };
+    },
+    btnStyles: function btnStyles() {
+      if (this.color && this.bordered) {
+        return {
+          color: this.color,
+          background: 'white',
+          border: "2px solid ".concat(this.color)
+        };
+      } else {
+        return {
+          background: this.color
+        };
+      }
+    },
+    getLoader: function getLoader() {
+      if (this.variant === "secondary") {
+        return __webpack_require__("fa5e");
+      }
+      return __webpack_require__("b9f2");
+    }
+  }
 });
 // CONCATENATED MODULE: ./src/components/Button/LitButton.vue?vue&type=script&lang=js&
  /* harmony default export */ var Button_LitButtonvue_type_script_lang_js_ = (LitButtonvue_type_script_lang_js_); 
-// EXTERNAL MODULE: ./src/components/Button/LitButton.vue?vue&type=style&index=0&id=bf61b33e&prod&scoped=true&lang=css&
-var LitButtonvue_type_style_index_0_id_bf61b33e_prod_scoped_true_lang_css_ = __webpack_require__("fb00");
+// EXTERNAL MODULE: ./src/components/Button/LitButton.vue?vue&type=style&index=0&id=42558152&prod&lang=scss&scoped=true&
+var LitButtonvue_type_style_index_0_id_42558152_prod_lang_scss_scoped_true_ = __webpack_require__("0bcb");
 
 // CONCATENATED MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
 /* globals __VUE_SSR_CONTEXT__ */
@@ -305,7 +417,7 @@ var component = normalizeComponent(
   staticRenderFns,
   false,
   null,
-  "bf61b33e",
+  "42558152",
   null
   
 )
@@ -322,17 +434,6 @@ var component = normalizeComponent(
 // CONCATENATED MODULE: ./node_modules/@vue/cli-service/lib/commands/build/entry-lib-no-default.js
 
 
-
-
-/***/ }),
-
-/***/ "fb00":
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_6_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_6_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_6_oneOf_1_2_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_LitButton_vue_vue_type_style_index_0_id_bf61b33e_prod_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("2da1");
-/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_6_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_6_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_6_oneOf_1_2_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_LitButton_vue_vue_type_style_index_0_id_bf61b33e_prod_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_mini_css_extract_plugin_dist_loader_js_ref_6_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_6_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_6_oneOf_1_2_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_LitButton_vue_vue_type_style_index_0_id_bf61b33e_prod_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__);
-/* unused harmony reexport * */
 
 
 /***/ })
